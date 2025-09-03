6d264f7e2b862dfc0c90be8bb9d941ac63286487
Nimbuswiztech <160088269+nimbuswiztech@users.noreply.github.com>
2025-09-03 20:30:38 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
-    access_key = "AKIA2UC3BCCG4RSZSVMC"
-    secret_key = "ngKei7CYBu3euZsjAUDOh5KgEL/ILlTqq3R9Pcns"
+    access_key = ""
+    secret_key = ""
 
 }
 
